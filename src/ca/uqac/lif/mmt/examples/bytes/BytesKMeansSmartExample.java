5f9702da609421faa5a4aec52ded9d897ac46cb9
Eva <evalukario@hotmail.fr>
2018-01-26 00:03:43 +0100

    documentation

--- src/ca/uqac/lif/mmt/examples/bytes/BytesKMeansSmartExample.java
+++ src/ca/uqac/lif/mmt/examples/bytes/BytesKMeansSmartExample.java
@@ -1,3 +1,18 @@
+/*
+    BeepBeep, an event stream processor
+    Copyright (C) 2008-2018 Sylvain Hallé
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published
+    by the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+    You should have received a copy of the GNU Lesser General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package ca.uqac.lif.mmt.examples.bytes;
 
 import ca.uqac.lif.cep.Connector;
@@ -9,6 +24,23 @@ import ca.uqac.lif.mmt.functions.GetSourceBytes;
 import ca.uqac.lif.mmt.functions.KMeansSmartFunction;
 import ca.uqac.lif.mmt.processors.*;
 
+/**
+ * An example of BeepBeep's usage in data mining: an application of improved K-Means algorithm on source and
+ * destination bytes in a connection log.
+ * <p>
+ *  The number of bytes sent and received for each analyzed connection go through the K-Means clustering algorithm,
+ *  a PNG image is then generated to display the resulting clusters on a graph.
+ * </p>
+ * <p>
+ *  This example illustrates the use of {@link KMeansSmartFunction} and {@link ScatterPlotGenerator} objects.
+ * </p>
+ * <p>
+ *  Represented graphically, this example corresponds to the following chain of processors:
+ * </p>
+ * <p>
+ *  <img src="{@docRoot}/img/MMT-BytesKMeansExample.png" alt="Processor graph">
+ * </p>
+ */
 public class BytesKMeansSmartExample {
 
     private static int k = 5;
@@ -17,31 +49,30 @@ public class BytesKMeansSmartExample {
 
     public static void main(String args[]) {
 
-        // Lire les connexions dans le fichier et les filtrer
+        /* Reading connections from source file and filter the data */
         FileSourceProcessor source = new FileSourceProcessor("./data/2006/11/20061101.txt");
         BytesFilterProcessor filter = new BytesFilterProcessor(limit);
         Fork fork = new Fork(2);
         Connector.connect(source, filter, fork);
 
-        // Extraction des paramètres intéressants
+        /* Extraction of the parameters of interest */
         FunctionProcessor sourceBytes = new FunctionProcessor(new GetSourceBytes());
         FunctionProcessor destinationBytes= new FunctionProcessor(new GetDestinationBytes());
         Connector.connect(fork, 0, sourceBytes,0);
         Connector.connect(fork, 1, destinationBytes,0);
 
-
-        // On construit une paire qui correspond aux coordonnées
+        /* Building a pair/coordinates object */
         PairBuilderProcessor pairBuilder = new PairBuilderProcessor();
         Connector.connect(sourceBytes, 0, pairBuilder, 0);
         Connector.connect(destinationBytes, 0, pairBuilder, 1);
 
-        // On construit un set de données exploitables pour KMeans
+        /* Building a data set that will be usable by KMeans function */
         SetBuilderProcessor setBuilder = new SetBuilderProcessor(k, refreshInterval);
         Connector.connect(pairBuilder, setBuilder);
 
-        // On applique KMeans
-        KMeansSmartFunction kmf2 = new KMeansSmartFunction(k);
-        FunctionProcessor fp = new FunctionProcessor(kmf2);
+        /* Applying KMeans function */
+        KMeansSmartFunction kmf = new KMeansSmartFunction(k);
+        FunctionProcessor fp = new FunctionProcessor(kmf);
 
         Connector.connect(setBuilder, fp);
 
