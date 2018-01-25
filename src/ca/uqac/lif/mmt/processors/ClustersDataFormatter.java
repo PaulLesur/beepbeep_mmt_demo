e1940c1f41e9a4fa875372fd8a4e1b216790130c
Paul Lesur <paul.lesur@free.fr>
2018-01-25 15:35:30 -0500

    Meilleur résultat visuel pour le KMeans  😤

--- src/ca/uqac/lif/mmt/processors/ClustersDataFormatter.java
+++ src/ca/uqac/lif/mmt/processors/ClustersDataFormatter.java
@@ -3,20 +3,18 @@ package ca.uqac.lif.mmt.processors;
 import ca.uqac.lif.cep.Processor;
 import ca.uqac.lif.cep.SingleProcessor;
 import ca.uqac.lif.cep.sets.Multiset;
-import com.sun.org.apache.xpath.internal.operations.Mult;
 import org.apache.commons.math3.ml.clustering.CentroidCluster;
 import org.apache.commons.math3.ml.clustering.DoublePoint;
-import org.jfree.chart.ChartFactory;
-import org.jfree.chart.ChartFrame;
-import org.jfree.chart.JFreeChart;
-import org.jfree.chart.plot.PlotOrientation;
 import org.jfree.data.xy.XYSeries;
 import org.jfree.data.xy.XYSeriesCollection;
 
-import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Queue;
 
+
+/**
+ * Processor that takes a Multiset and format it to XYSeriesCollection
+ */
 public class ClustersDataFormatter  extends SingleProcessor{
     public ClustersDataFormatter() {
         super(1,1);
@@ -25,34 +23,30 @@ public class ClustersDataFormatter  extends SingleProcessor{
     @Override
     protected boolean compute(Object[] inputs, Queue<Object[]> output) {
 
+        // Input
         Multiset inSet = (Multiset) inputs[0];
 
+        // The XYSeriesCollection returned
         XYSeriesCollection result = new XYSeriesCollection();
 
-
-//            CentroidCluster<DoublePoint> currentCluster = (CentroidCluster<DoublePoint>)inArray[i];
-//            System.out.println(i+" : "+currentCluster.toString());
-
+        // Iterator used to iterate over the input Multiset
         Iterator iteratorSet = inSet.iterator();
 
         int counter = 0;
+
+        // We iterate over the input Multiset
         while(iteratorSet.hasNext()){
-//                System.out.println(iteratorSet.next());
-
             CentroidCluster<DoublePoint> currentCluster = (CentroidCluster<DoublePoint>) iteratorSet.next();
-//            System.out.println(currentCluster);
-
             XYSeries series = new XYSeries(String.valueOf(counter));
             int nbPointsOfCurrentCluster = currentCluster.getPoints().size();
 
+            // Iterate over each DoublePoint of the cluster
             for (int i = 0; i<nbPointsOfCurrentCluster; i++ ){
-
-//                System.out.println(counter+":"+i);
                 DoublePoint temp = (DoublePoint) currentCluster.getPoints().get(i);
-
                 double x = temp.getPoint()[0];
                 double y = temp.getPoint()[1];
 
+                // We add the DoublePoint in the
                 series.add(x, y);
             }
 
@@ -60,18 +54,6 @@ public class ClustersDataFormatter  extends SingleProcessor{
             result.addSeries(series);
             counter++;
         }
-
-//
-//
-//            DoublePoint temp = (DoublePoint) inArray[i];
-//
-//            double x = temp.getPoint()[0];
-//            double y = temp.getPoint()[1];
-//
-//            series.add(x, y);
-
-
-
 
 
         Object[]out = {result};
